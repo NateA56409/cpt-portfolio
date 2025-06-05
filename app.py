88f9e1df004669758a85ef09ad87e9cab6859480
NateA56409 <nathaylsworth@gmail.com>
2025-06-05 09:15:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,8 @@ def project_detail():
     return render_template('project.html', project=project)
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+if __name__ == '__main__':
     app.run(debug=True)
